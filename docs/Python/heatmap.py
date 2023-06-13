0d44e6b13586a106114dee1f6dd3c757bee5e1b7
Ellie Nausicaa Dorland <133685766+edorland1@users.noreply.github.com>
2023-06-13 20:34:31 +0000

    CSS changes

--- docs/Python/heatmap.py
+++ docs/Python/heatmap.py
@@ -1,11 +1,2 @@
-'''this should show a map'''
-import numpy as np
-import matplotlib.pyplot as plt
-
-# Create a 2D NumPy array of random values
-image = np.random.rand(100, 100)
-
-# Display the image using Matplotlib
-plt.imshow(image, cmap='gray')
-plt.axis('off')  # Hide the axes
-plt.show()
+hello = "hI, my name is aaaahhah"
+print (hello)
