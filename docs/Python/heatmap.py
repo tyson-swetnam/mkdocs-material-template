aa85c9d3e2e3aed7d53d9f818bad7a0251dc0a50
Ellie Nausicaa Dorland <133685766+edorland1@users.noreply.github.com>
2023-06-13 21:45:48 +0000

    Attempting a python user interface on the website

--- docs/Python/heatmap.py
+++ docs/Python/heatmap.py
@@ -1,2 +1,7 @@
-hello = "hI, my name is aaaahhah"
-print (hello)
+import folium
+
+# Create a map object
+map_object = folium.Map(location=[37.7749, -122.4194], zoom_start=12)
+
+# Display the map
+map_object.save('map.html')
